b507dae503960110d7632b6f19421852ba918742
AlecJYip <60280704+AlecJYip@users.noreply.github.com>
2022-05-30 22:26:56 -0400

    Update Figure_3.m

--- Figure_3.m
+++ Figure_3.m
@@ -22,6 +22,8 @@ F = 0.4*D;
 
 theta_rim_max = pi/2;
 
+%theta_rim_max = 2*pi/180;
+
 P = zeros(1,length(0:theta_rim_max/200:theta_rim_max));
 
 theta_rim = 0;
@@ -41,13 +43,16 @@ while(theta_rim<=theta_rim_max)
     while(theta<=theta_rim)        
         phi =0;
         p = 2*F*tan(theta/2);
+        rf = F*(sec(theta/2)^2);
+        zf = sqrt(p^2-rf^2);
         while(phi<=2*pi)
             term1 = (cos(theta)^q)/sqrt((cos(theta)^2)*(sin(phi)^2)+(cos(phi)^2));
-            term2 = sqrt(...
+            term2 = sqrt(abs(...
                 (cos(theta)*sin(phi)*cos(theta/2))^2+...
                 (cos(phi)*cos(theta/2))^2+...
-                (cos(theta)*sin(phi)*sin(theta/2))^2);
+                (cos(theta)*sin(phi)*sin(theta/2))^2));
             term3 = exp(1i*beta*p*sin(theta)*cos(phi));
+            %term3 = exp(-1i*beta*zf);
             func = term1*term2*term3;
             arr = arr + (step_size^2)*func;
             phi = phi + step_size;
@@ -63,4 +68,4 @@ end
 %% Plotting
 angle_rim = (0:theta_rim_max/200:theta_rim_max);
 D_dBi = 10*log10(4*pi./(4*pi*(abs(P).^2)));
-figure;plot(angle_rim*180/pi,D_dBi);hold all;
+figure;plot(angle_rim*180/pi,D_dBi);hold all;xlabel('\theta');ylabel('Directivity [dBi]');title('Figure 3');grid on;
