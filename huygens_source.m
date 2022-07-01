e2076ff3b8f72c4da520b02263e296584fd4de25
AlecJYip <60280704+AlecJYip@users.noreply.github.com>
2022-07-01 01:29:36 -0400

    Update huygens_source.m

--- huygens_source.m
+++ huygens_source.m
@@ -1,3 +1,7 @@
+%% Title Block
+%Purpose of code: To plot the co-polarized pattern function of the
+%dipole-consisted huygen's source for any constanth theta or phi
+
 clear;
 close all;
 clc;
@@ -10,41 +14,76 @@ l_h_h = [1 0 0]; %unit vector for the magnetic dipole
 
 %% Huygen's Source Analysis
 
-Angle = 1; 
+Angle = 1; %1 for theta, 2 for phi
 
 if(Angle == 1)
-    theta = 0 * pi/180; %rad
+    theta = 45 * pi/180; %rad, angle specified by user
     phi = 0; %rad, looping through phi angle
-    F =  zeros(length(0:d_phi:2*pi),1); %initializing the scattered E field as a function of theta
+    F =  zeros(1,length(0:d_phi:2*pi)); %initializing the scattered E field as a function of theta
     g = 1; %index counter
     while(phi<=2*pi)
         s_i_h = [sin(theta)*cos(phi) sin(theta)*sin(phi) cos(theta)]; %radial unit vector from the center of the dipoles to the field point
-        F(g,:) = 0.5*norm(cross(cross(l_e_h, s_i_h), s_i_h)+cross(l_h_h, s_i_h)); %normalized pattern function of the two dipoles
-        g = g + 1;
-        phi = phi + d_phi;
-    end
-    
+        E_p = cross(cross(l_e_h, s_i_h), s_i_h);
+        H_p = cross(l_h_h, s_i_h);
+        F(g) = norm(0.5*H_p+0.5*E_p);        
+        g = g + 1; %array counter
+        phi = phi + d_phi; %incrementing phi angle by 1/100th of a degree
+    end  
 
     phi_angle = (0:d_phi:2*pi); %rad, used to set the axis of the plot
-    figure;plot(phi_angle, 20*log10((F))); hold all; grid on; ylabel('Magnitude [dB]')
-    xlabel('\phi [rad]'); xlim([0 2*pi]); ylim([-120 0]);
-    figure;polarplot(F);
+    %figure;plot(phi_angle, 20*log10(abs(F))); hold all; grid on; ylabel('Magnitude [dB]')
+    %xlabel('\phi [rad]'); xlim([0 2*pi]); ylim([-120 0]);
+    figure;polarplot(F);title('\theta[deg] = ', theta*180/pi);rlim([0 1])
+    
+    theta = 135 * pi/180; %rad, angle specified by user
+    phi = 0; %rad, looping through phi angle
+    F =  zeros(1,length(0:d_phi:2*pi)); %initializing the scattered E field as a function of theta
+    g = 1; %index counter
+    while(phi<=2*pi)
+        s_i_h = [sin(theta)*cos(phi) sin(theta)*sin(phi) cos(theta)]; %radial unit vector from the center of the dipoles to the field point
+        E_p = cross(cross(l_e_h, s_i_h), s_i_h);
+        H_p = cross(l_h_h, s_i_h);
+        F(g) = 0.5*norm(E_p+H_p);             
+        g = g + 1; %array counter
+        phi = phi + d_phi; %incrementing phi angle by 1/100th of a degree
+    end  
+
+    %figure;plot(phi_angle, 20*log10(abs(F))); hold all; grid on; ylabel('Magnitude [dB]')
+    %xlabel('\phi [rad]'); xlim([0 2*pi]); ylim([-120 0]);
+    figure;polarplot(F);title('\theta[deg] = ', theta*180/pi); ;rlim([0 1])
     
 elseif(Angle == 2)
-    phi = 70*pi/180; %rad
+    phi = 180*pi/180; %rad, angle specified by user
     theta = 0;%rad, looping through theta angle
-    F =  zeros(length(0:d_theta:2*pi),1); %initializing the scattered E field as a function of theta
+    F =  zeros(length(0:d_theta:2*pi),3); %initializing the scattered E field as a function of theta
     g = 1;%index counter
     while(theta<=2*pi)
         s_i_h = [sin(theta)*cos(phi) sin(theta)*sin(phi) cos(theta)]; %radial unit vector from the center of the dipoles to the field point
-        F(g,:) = 0.5*norm(cross(cross(l_e_h, s_i_h), s_i_h)+cross(l_h_h, s_i_h)); %normalized pattern function of the two dipoles
-        g = g + 1;
-        theta = theta + d_theta;
+        E_p = cross(cross(l_e_h, s_i_h), s_i_h);
+        H_p = cross(l_h_h, s_i_h);
+        F(g) = 0.5*norm(E_p+H_p);        
+        g = g + 1; %array counter
+        theta = theta + d_theta; %incrementing theta angle by 1/100th of a degree
     end
     theta_angle = (0:d_theta:2*pi);
-    figure;plot(theta_angle, 20*log10((F))); hold all; grid on; ylabel('Magnitude [dB]')
-    xlabel('\theta [rad]'); xlim([0 2*pi]);ylim([-120 0]);
-    figure;polarplot(F);
+    %figure;plot(theta_angle, 20*log10(abs(F))); hold all; grid on; ylabel('Magnitude [dB]')
+    %xlabel('\theta [rad]'); xlim([0 2*pi]);ylim([-120 0]);
+    figure;polarplot(F); title('\phi [deg] = ', phi*180/pi);
+    
+    phi = 270*pi/180; %rad, angle specified by user
+    theta = 0;%rad, looping through theta angle
+    g = 1;%index counter
+    while(theta<=2*pi)
+        s_i_h = [sin(theta)*cos(phi) sin(theta)*sin(phi) cos(theta)]; %radial unit vector from the center of the dipoles to the field point
+        E_p = cross(cross(l_e_h, s_i_h), s_i_h);
+        H_p = cross(l_h_h, s_i_h);
+        F(g) = 0.5*norm(H_p+E_p);        
+        g = g + 1; %array counter
+        theta = theta + d_theta; %incrementing theta angle by 1/100th of a degree
+    end
+    %figure;plot(theta_angle, 20*log10(abs(F))); hold all; grid on; ylabel('Magnitude [dB]')
+    %xlabel('\theta [rad]'); xlim([0 2*pi]);ylim([-120 0]);
+    figure;polarplot(F); title('\phi [deg] = ', phi*180/pi);
     
 else
     disp('Check input')
@@ -53,24 +92,20 @@ end
 
 %% Directivity
 
-%Directivity
-theta = 0;
-range_theta = (0:d_theta:pi);
-range_phi = (0:d_phi:2*pi);
-arr = zeros(1,length(range_theta).*length(range_phi));
-n = 1;
+%Following code is for calculating the directivity of the huygen's source
+%via the beam-solid-angle method
+
+theta = 0; %initializing thetas angle
+b_S_A = 0; %initializing beam solid angle sum
 while(theta<=pi)
     phi =0;
     while(phi<=2*pi)
         s_i_h = [sin(theta)*cos(phi) sin(theta)*sin(phi) cos(theta)]; %radial unit vector from the center of the dipoles to the field point
         F = 0.5*norm(cross(cross(l_e_h, s_i_h), s_i_h)+cross(l_h_h, s_i_h));%normalized pattern function of the two dipoles
-        func = sin(theta)*(abs(F)^2);
-        arr(n) = func*d_theta*d_phi;
-        phi = phi + d_phi;
-        n = n + 1;
+        b_S_A = b_S_A+(abs(F)^2)*sin(theta)*d_theta*d_phi; %mupltiplying by differential aray, sin(theta)*d_theta*d_phi
+        phi = phi + d_phi; %incrementing phi by  1/100th of a degree
     end
-    theta = theta + d_theta;
+    theta = theta + d_theta; %incrementing theta by  1/100th of a degree
 end
 
-solid_angle = sum(arr);
-D_dBi = 10*log10(4*pi/solid_angle)
+D_dBi = 10*log10(4*pi/b_S_A)
